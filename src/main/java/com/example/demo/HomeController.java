97956f0300cdc851065816bffaca34f7cebc97db
Nick Boober <nboober@terpmail.umd.edu>
2019-08-17 18:33:27 -0400

    trying to pull the id from the combined table to use later. Getting close

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -30,7 +30,7 @@ public class HomeController {
     }
 
     @RequestMapping("/processOrderDate")
-    public String processOrderDate(@Valid OrderDate orderDate, @ModelAttribute OrderCombined orderCombined,
+    public String processOrderDate(@Valid OrderDate orderDate, @RequestParam("combinedId") long order_id, @ModelAttribute OrderCombined orderCombined,
 //                                   @RequestParam("orderDateId") long order_id,
                                    BindingResult result, Model model){
         if(result.hasErrors()){
@@ -41,41 +41,44 @@ public class HomeController {
         orderDateRepository.save(orderDate);
         orderCombined.setOrderDate(orderDate);
         orderCombinedRepository.save(orderCombined);
-        return "redirect:/orderDetail";
+        return "redirect:/orderDetail/"+ order_id;
 //        return "redirect:/orderDetail/" + order_id;
 
     }
 
 //    @RequestMapping("/orderDetail/{id}")
-    @RequestMapping("/orderDetail")
-    public String orderDetail(
+    @RequestMapping("/orderDetail/{order_id}")
+    public String orderDetail(@PathVariable("order_id") long order_id,
             //@PathVariable("id") long order_id,
                               Model model){
 
         model.addAttribute("orderDetail", new OrderDetail());
+        model.addAttribute("orderCombined", orderCombinedRepository.findById(order_id).get());
 //        model.addAttribute("orderDate", orderDateRepository.findById(order_id).get());
 
         return "orderDetail";
     }
 
     @RequestMapping("/processOrderDetails")
-    public String processOrderDetails(@Valid OrderDetail orderDetail, @ModelAttribute("orderCombined") OrderCombined orderCombined,
+    public String processOrderDetails(@Valid OrderDetail orderDetail, @RequestParam("combinedId") long order_id, @ModelAttribute("orderCombined") OrderCombined orderCombined,
                                       //@RequestParam("orderDateId") long order_id,
                                       BindingResult result, Model model){
         if(result.hasErrors()){
+            model.addAttribute("orderCombined", orderCombinedRepository.findById(order_id).get());
             model.addAttribute("orderDetail", orderDetail);
             return "orderDetail";
         }
 
 //        OrderDate orderDate = orderDateRepository.findById(order_id).get();
+        OrderCombined combined = orderCombinedRepository.findById(order_id).get();
 
 
         orderDetailRepository.save(orderDetail);
 //        orderCombined.setOrder_date(orderDate.getOrder_date());
-        orderCombined.setOrderDetail(orderDetail);
-        orderCombined.setOrder_amount(orderDetail.getOrder_amount());
-        orderCombined.setOrder_description(orderDetail.getOrder_description());
-        orderCombinedRepository.save(orderCombined);
+        combined.setOrderDetail(orderDetail);
+        combined.setOrder_amount(orderDetail.getOrder_amount());
+        combined.setOrder_description(orderDetail.getOrder_description());
+        orderCombinedRepository.save(combined);
 
         return "redirect:/";
 
